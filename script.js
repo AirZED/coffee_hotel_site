ab3d31c34ff15f37f0a68c7f8827198391397029
AirZED <regnalukpabio@gmail.com>
2022-01-15 23:54:02 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick =() =>{
+    navbar.classList.toggle('active');
+    searchBtn.classList.remove('active');
+    cartItem.classList.remove('active');
+} 
+
+let cartItem = document.querySelector('.cart-items-container')
+
+document.querySelector('#cart-btn').onclick =() =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchBtn.classList.remove('active');
+} 
+
+
+let searchBtn = document.querySelector('.search-form')
+
+document.querySelector('#search-btn').onclick =() =>{
+    searchBtn.classList.toggle('active');
+    cartItem.classList.remove('active');
+    navbar.classList.remove('active');
+} 
+
+window.onscroll = ()=>{
+
+    navbar.classList.remove('active');
+    searchBtn.classList.remove('active');
+    cartItem.classList.remove('active');
+}
